f73352a1b641dcf7de2b889146eac7599907d316
alvessergio <juniorspse@gmail.com>
2016-10-06 15:57:04 -0300

    Alterações na tela Profissao

--- HotelCetafet/View/FormProfissao.Designer.cs
+++ HotelCetafet/View/FormProfissao.Designer.cs
@@ -28,20 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProfissao));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.Label descricaoLabel;
+            System.Windows.Forms.Label label1;
+            this.bdHotelCetafestDataSet = new HotelCetafet.bdHotelCetafestDataSet();
+            this.profissaoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.profissaoTableAdapter = new HotelCetafet.bdHotelCetafestDataSetTableAdapters.ProfissaoTableAdapter();
+            this.tableAdapterManager = new HotelCetafet.bdHotelCetafestDataSetTableAdapters.TableAdapterManager();
+            this.profissaoBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.profissaoBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.profissaoDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.descricaoTextBox = new System.Windows.Forms.TextBox();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            descricaoLabel = new System.Windows.Forms.Label();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bdHotelCetafestDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profissaoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profissaoBindingNavigator)).BeginInit();
+            this.profissaoBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.profissaoDataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bdHotelCetafestDataSet
+            // 
+            this.bdHotelCetafestDataSet.DataSetName = "bdHotelCetafestDataSet";
+            this.bdHotelCetafestDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // profissaoBindingSource
+            // 
+            this.profissaoBindingSource.DataMember = "Profissao";
+            this.profissaoBindingSource.DataSource = this.bdHotelCetafestDataSet;
+            // 
+            // profissaoTableAdapter
+            // 
+            this.profissaoTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ClienteTableAdapter = null;
+            this.tableAdapterManager.PaisTableAdapter = null;
+            this.tableAdapterManager.ProfissaoTableAdapter = this.profissaoTableAdapter;
+            this.tableAdapterManager.UpdateOrder = HotelCetafet.bdHotelCetafestDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // profissaoBindingNavigator
+            // 
+            this.profissaoBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.profissaoBindingNavigator.BindingSource = this.profissaoBindingSource;
+            this.profissaoBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.profissaoBindingNavigator.DeleteItem = null;
+            this.profissaoBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.profissaoBindingNavigatorSaveItem,
+            this.toolStripButton1});
+            this.profissaoBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.profissaoBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.profissaoBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.profissaoBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.profissaoBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.profissaoBindingNavigator.Name = "profissaoBindingNavigator";
+            this.profissaoBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.profissaoBindingNavigator.Size = new System.Drawing.Size(535, 25);
+            this.profissaoBindingNavigator.TabIndex = 0;
+            this.profissaoBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 22);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(56, 22);
+            this.bindingNavigatorAddNewItem.Text = "Novo";
+            // 
+            // profissaoBindingNavigatorSaveItem
+            // 
+            this.profissaoBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("profissaoBindingNavigatorSaveItem.Image")));
+            this.profissaoBindingNavigatorSaveItem.Name = "profissaoBindingNavigatorSaveItem";
+            this.profissaoBindingNavigatorSaveItem.Size = new System.Drawing.Size(58, 22);
+            this.profissaoBindingNavigatorSaveItem.Text = "Salvar";
+            this.profissaoBindingNavigatorSaveItem.Click += new System.EventHandler(this.profissaoBindingNavigatorSaveItem_Click);
+            // 
+            // profissaoDataGridView
+            // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Silver;
+            this.profissaoDataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.profissaoDataGridView.AutoGenerateColumns = false;
+            this.profissaoDataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.profissaoDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.profissaoDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.profissaoDataGridView.DataSource = this.profissaoBindingSource;
+            this.profissaoDataGridView.Location = new System.Drawing.Point(12, 116);
+            this.profissaoDataGridView.Name = "profissaoDataGridView";
+            this.profissaoDataGridView.Size = new System.Drawing.Size(511, 301);
+            this.profissaoDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "codigo";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Codigo";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "descricao";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Descricao";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(61, 22);
+            this.toolStripButton1.Text = "Excluir";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // descricaoLabel
+            // 
+            descricaoLabel.AutoSize = true;
+            descricaoLabel.Location = new System.Drawing.Point(12, 38);
+            descricaoLabel.Name = "descricaoLabel";
+            descricaoLabel.Size = new System.Drawing.Size(58, 13);
+            descricaoLabel.TabIndex = 4;
+            descricaoLabel.Text = "Descricao:";
+            // 
+            // descricaoTextBox
+            // 
+            this.descricaoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.profissaoBindingSource, "descricao", true));
+            this.descricaoTextBox.Location = new System.Drawing.Point(15, 54);
+            this.descricaoTextBox.Name = "descricaoTextBox";
+            this.descricaoTextBox.Size = new System.Drawing.Size(508, 20);
+            this.descricaoTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(266, 93);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(53, 13);
+            label1.TabIndex = 15;
+            label1.Text = "Pesquisa:";
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(325, 90);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(198, 20);
+            this.txtPesquisa.TabIndex = 14;
+            this.txtPesquisa.TextChanged += new System.EventHandler(this.txtPesquisa_TextChanged);
             // 
             // FormProfissao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 338);
+            this.ClientSize = new System.Drawing.Size(535, 440);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(descricaoLabel);
+            this.Controls.Add(this.descricaoTextBox);
+            this.Controls.Add(this.profissaoDataGridView);
+            this.Controls.Add(this.profissaoBindingNavigator);
             this.Name = "FormProfissao";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gerenciamento de Profissões";
+            this.Load += new System.EventHandler(this.FormProfissao_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bdHotelCetafestDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profissaoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.profissaoBindingNavigator)).EndInit();
+            this.profissaoBindingNavigator.ResumeLayout(false);
+            this.profissaoBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.profissaoDataGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private bdHotelCetafestDataSet bdHotelCetafestDataSet;
+        private System.Windows.Forms.BindingSource profissaoBindingSource;
+        private bdHotelCetafestDataSetTableAdapters.ProfissaoTableAdapter profissaoTableAdapter;
+        private bdHotelCetafestDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator profissaoBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton profissaoBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView profissaoDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TextBox descricaoTextBox;
+        private System.Windows.Forms.TextBox txtPesquisa;
     }
 }
--- HotelCetafet/View/FormProfissao.cs
+++ HotelCetafet/View/FormProfissao.cs
@@ -16,5 +16,62 @@ namespace HotelCetafet.View
         {
             InitializeComponent();
         }
+
+        private void profissaoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.profissaoBindingSource.EndEdit();
+                this.tableAdapterManager.UpdateAll(this.bdHotelCetafestDataSet);
+                Msg.sucesso("Dados salvos com sucesso.");
+                this.profissaoTableAdapter.Fill(this.bdHotelCetafestDataSet.Profissao);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("null"))
+                {
+                    Msg.atencao("Preencha todos os campos.");
+                }
+                else
+                {
+                    Msg.erro("Não foi possivel salvar os dados.\nO sistema não está conseguindo se conectar com o servidor!");
+                }
+            }
+
+        }
+
+        private void FormProfissao_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bdHotelCetafestDataSet.Profissao' table. You can move, or remove it, as needed.
+            this.profissaoTableAdapter.Fill(this.bdHotelCetafestDataSet.Profissao);
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (Msg.simNao("Realmente Deseja Excluir?") == DialogResult.Yes)
+            {
+
+                try
+                {
+                    this.profissaoBindingSource.RemoveCurrent();
+                    this.Validate();
+                    this.profissaoBindingSource.EndEdit();
+                    this.tableAdapterManager.UpdateAll(this.bdHotelCetafestDataSet);
+                    Msg.sucesso("Removido com sucesso.");
+                    this.profissaoTableAdapter.Fill(this.bdHotelCetafestDataSet.Profissao);
+                }
+                catch (Exception ex)
+                {
+                    Msg.erro("Não foi possivel remover os dados.\nO sistema não está conseguindo se conectar com o servidor!");
+                }
+            }
+        }
+
+        private void txtPesquisa_TextChanged(object sender, EventArgs e)
+        {
+            profissaoBindingSource.Filter = "descricao like '%"+txtPesquisa.Text+"%'";
+        }
     }
 }
